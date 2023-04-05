5a3c27c1128a4c0065a90c6fee01d9268e1250f1
Razvan Paros <razvanparos21@gmail.com>
2023-04-05 21:31:23 +0300

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TitleComponent } from './title/title.component';
+import { NumbersComponent } from './numbers/numbers.component';
+import { ContainerComponent } from './container/container.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TitleComponent,
+    NumbersComponent,
+    ContainerComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
